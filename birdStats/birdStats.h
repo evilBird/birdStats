c094d1330fa6bde8054c473808c848ee116ace2b
evilBird <travishenspeter@traviss-macbook-pro.local>
2015-12-08 14:35:27 -0600

    Initial Commit

---
+++ birdStats/birdStats.h
@@ -0,0 +1,13 @@
+//
+//  birdStats.h
+//  birdStats
+//
+//  Created by Travis Henspeter on 12/8/15.
+//  Copyright © 2015 birdSound. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface birdStats : NSObject
+
+@end
---
+++ birdStats/birdStats.m
@@ -0,0 +1,13 @@
+//
+//  birdStats.m
+//  birdStats
+//
+//  Created by Travis Henspeter on 12/8/15.
+//  Copyright © 2015 birdSound. All rights reserved.
+//
+
+#import "birdStats.h"
+
+@implementation birdStats
+
+@end
