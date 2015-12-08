0bc97c8fd352ac939e68234f8d3354a4230495a2
evilBird <travishenspeter@Traviss-MacBook-Pro.local>
2015-12-08 17:57:29 -0600

    add summary stats and random normal functions

--- birdStats/src/birdBuffer.h
+++ birdStats/src/birdBuffer.h
@@ -12,18 +12,32 @@
 #include <stdio.h>
 
 typedef struct {
-    int n;
-    int i;
-    int max;
-    double *observations;
+    int n; //current number of observations
+    int i; //index at which the next observation will be added
+    int max; //maximum number of observations
+    char tag[100];
+    double *observations; //an array of observations
 } birdBuffer;
 
-void    createBuffer (birdBuffer *buffer, int max);
+void    createBuffer (birdBuffer *buffer, char *tag, int max);
 void    addToBuffer (birdBuffer *buffer, double obs);
+double  getBufferObs (birdBuffer *buffer, int idx); //get the observation at index 'idx' where idx = 0 is the oldest and idx = ( n - 1 ) is the most recent
+double  getBufferRecentObs (birdBuffer *buffer, int idx); //get the observation at index 'idx' where idx = 0 is the most recent and ( n - 1 ) is the newest
+double  getBufferQuartile (birdBuffer *buffer, int q);
 double  getBufferMin(birdBuffer *buffer);
 double  getBufferMax (birdBuffer *buffer);
 double  getBufferMean (birdBuffer *buffer);
+double  getBufferWeightedMean (birdBuffer *buffer);
 double  getBufferMedian (birdBuffer *buffer);
+double  getBufferSSE (birdBuffer *buffer);
+double  getBufferMSE (birdBuffer *buffer);
+double  getBufferSD (birdBuffer *buffer);
+void    setBuffer (birdBuffer *buffer, double setToValue);
+void    setBufferObs (birdBuffer *buffer, double value, int idx); //set the value of the observation at index 'idx' to specified value, where idx = 0 is the oldest and idx = ( n - 1 ) is the most recent
+void    zeroBuffer (birdBuffer *buffer);
 void    destroyBuffer (birdBuffer *buffer);
-
+void    printBufferSummary (birdBuffer *buffer);
+double  getPDF(double value, double mu, double sigma);
+double  getRandomNormal(double mu, double sigma);
+double  getBufferPDF(birdBuffer *buffer, double value);
 #endif /* birdBuffer_h */
--- birdStatsTests/birdStatsTests.m
+++ birdStatsTests/birdStatsTests.m
@@ -20,8 +20,9 @@
 
 - (void)setUp {
     [super setUp];
-    _n = 10;
-    createBuffer(&_birdBuffer, _n);
+    _n = 100;
+    NSString *tag = @"birdStats Tests";
+    createBuffer(&_birdBuffer, [tag UTF8String],_n);
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
@@ -35,15 +36,111 @@
 - (void)testBufferMean
 {
     double runningSum = 0;
-    for (int i = 0;i<self.n;i++) {
+    int offset = self.n*0.4;
+    int num = self.n+offset;
+    for (int i = 0;i<num;i++) {
         double obs = (double)(i * arc4random_uniform(100));
         addToBuffer(&_birdBuffer, obs);
-        runningSum+=obs;
+        if ( i >= offset) {
+            runningSum+=obs;
+        }
     }
     
     double expectedResult = runningSum/(double)self.n;
     double result = getBufferMean(&_birdBuffer);
     XCTAssertEqual(result, expectedResult);
+}
+
+- (void)testBufferGetObs
+{
+    int idx = 0;
+    double expectedResult = -1;
+    int offset = self.n*0.4;
+    int num = self.n+offset;
+    for (int i = 0;i<num;i++) {
+        double obs = (double)(i * arc4random_uniform(100));
+        addToBuffer(&_birdBuffer, obs);
+        if ( ( i - offset + idx ) == idx ) {
+            
+            expectedResult = obs;
+        }
+    }
+    
+    double result = getBufferObs(&_birdBuffer, idx);
+    
+    XCTAssertEqual(result, expectedResult);
+}
+
+- (void)testBufferRecentObs
+{
+    int idx = 88;
+    int offset = self.n*0.4;
+    int num = self.n+offset;
+    double expectedResult = -1.0;
+    for (int i = 0;i<num;i++) {
+        double obs = (double)(i * arc4random_uniform(100));
+        addToBuffer(&_birdBuffer, obs);
+        if ( ( num - 1 - i ) == idx ) {
+            expectedResult = obs;
+        }
+    }
+    
+    double result = getBufferRecentObs(&_birdBuffer, idx);
+    XCTAssertEqual(result, expectedResult);
+}
+
+- (void)testBufferVariance
+{
+    double runningSum = 0;
+    int offset = self.n*0.6;
+    int num = self.n+offset;
+    double theObs[num];
+    for (int i = 0;i<num;i++) {
+        double obs = (double)(i * arc4random_uniform(100));
+        addToBuffer(&_birdBuffer, obs);
+        if ( i >= offset) {
+            theObs[(i-offset)] = obs;
+            runningSum+=obs;
+        }
+    }
+    
+    double mean = getBufferMean(&_birdBuffer);
+    double sse = 0.0;
+    for (int i = 0; i < self.n; i++) {
+        double e = theObs[i]-mean;
+        double se = e*e;
+        sse+=se;
+    }
+    
+    double expectedSSE = sse;
+    double expectedMSE = sse/(double)self.n;
+    double actualSSE = getBufferSSE(&_birdBuffer);
+    double actualMSE = getBufferMSE(&_birdBuffer);
+    XCTAssertEqual(actualSSE, expectedSSE);
+    XCTAssertEqual(actualMSE, expectedMSE);
+}
+
+-(void)testBufferWeightedMean
+{
+    int offset = self.n*0.4;
+    int num = self.n+offset;
+    double sumOfWeights = 0.0;
+    double sumOfWeightedObs = 0.0;
+    for (int i = 0;i<num;i++) {
+        double obs = (double)(i * arc4random_uniform(100));
+        addToBuffer(&_birdBuffer, obs);
+        if ( i >= offset) {
+            int index = ( i - offset + 1 );
+            double weight = ((double)index/(double)_n);
+            NSLog(@"INDEX: %d, WEIGHT: %.2f",index,weight);
+            sumOfWeightedObs += (obs * weight);
+            sumOfWeights += weight;
+        }
+    }
+    
+    double expectedResult = sumOfWeightedObs/sumOfWeights;
+    double result = getBufferWeightedMean(&_birdBuffer);
+    XCTAssertEqual(round(result), round(expectedResult));
 }
 
 - (void)testBufferMedian
@@ -59,6 +156,28 @@
     XCTAssertEqual(result, expectedResult);
 }
 
+- (void)testBufferSet
+{
+    double runningSum = 0;
+    int offset = self.n*0.4;
+    int num = self.n+offset;
+    for (int i = 0;i<num;i++) {
+        double obs = (double)(i * arc4random_uniform(100));
+        addToBuffer(&_birdBuffer, obs);
+        if ( i >= offset) {
+            runningSum+=obs;
+        }
+    }
+    
+    double expectedResult = runningSum/(double)self.n;
+    double result = getBufferMean(&_birdBuffer);
+    XCTAssertEqual(result, expectedResult);
+    double setTo = -1.0;
+    setBuffer(&_birdBuffer, setTo);
+    double newMean = getBufferMean(&_birdBuffer);
+    XCTAssertEqual(newMean, setTo);
+}
+
 - (void)testBufferOrder
 {
     int num = self.n+1;
@@ -70,6 +189,36 @@
     double expectedResult = num-1;
     double result = _birdBuffer.observations[0];
     XCTAssertEqual(result, expectedResult);
+}
+
+- (void)testPrintBufferSummaryStats
+{
+    double runningSum = 0;
+    int offset = self.n*0.4;
+    int num = self.n+offset;
+    for (int i = 0;i<num;i++) {
+        double obs = (double)arc4random_uniform(100);
+        addToBuffer(&_birdBuffer, obs);
+    }
+    
+    printBufferSummary(&_birdBuffer);
+    double pdf = getBufferPDF(&_birdBuffer, 50.0);
+}
+
+- (void)testGetPDF
+{
+    double mean = 10.0;
+    double sd = 5.0;
+    for (int i = 0; i < self.n; i ++) {
+        double rnorm = getRandomNormal(mean, sd);
+        NSLog(@"norm: %.2f",rnorm);
+        addToBuffer(&_birdBuffer, rnorm);
+    }
+    
+    double mu = getBufferMean(&_birdBuffer);
+    double sig = getBufferSD(&_birdBuffer);
+    printBufferSummary(&_birdBuffer);
+    double pdf = getBufferPDF(&_birdBuffer, 5.0);
 }
 
 - (void)testExample {
